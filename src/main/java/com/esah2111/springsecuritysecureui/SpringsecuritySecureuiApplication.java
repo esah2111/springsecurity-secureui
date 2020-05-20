0c4e9b790d7e64efa31e2087810f8169be06f49c
Mohammad Esah <esah2111@gmail.com>
2020-05-20 19:48:30 +0530

    Initial Commit

---
+++ src/main/java/com/esah2111/springsecuritysecureui/SpringsecuritySecureuiApplication.java
@@ -0,0 +1,13 @@
+package com.esah2111.springsecuritysecureui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritySecureuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecuritySecureuiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esah2111/springsecuritysecureui/SpringsecuritySecureuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esah2111.springsecuritysecureui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritySecureuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
